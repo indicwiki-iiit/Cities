8310da623414a8e1c8af4339987503735987ef8e
Saketh Reddy <sakethreddy1305@gmail.com>
2021-05-24 17:33:04 +0530

    updated comments

--- data/readData.py
+++ data/readData.py
@@ -1,6 +1,7 @@
 import pickle
 import pandas as pd
 def main():
+	# Change the data set file here for different versions of project
 	citiesFile = './cities2.csv'
 	citiesDF = pd.read_csv(citiesFile)
 	pickle.dump(citiesDF, open('./citiesDF.pkl', 'wb'))
